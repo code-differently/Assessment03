a0313e23bc43a055b4a4b227f24e4d94912b53db
Lissette Garcia <lissettegarcia@Lissettes-MacBook-Air.local>
2020-07-20 13:56:05 -0500

    Assessment 3 done

--- src/main/java/com/stayready/as03/problem01/Problem01.java
+++ src/main/java/com/stayready/as03/problem01/Problem01.java
@@ -3,6 +3,23 @@ package com.stayready.as03.problem01;
 public class Problem01 {
 
     public String isThisWeird(int input){
+        for(int i = 0; i < input; i++ ) {
+            if(input % 2 == 0) {
+                if(input >= 2 && input <= 5){
+                    if(input >= 6 && input <= 20){
+                        if(input >= 21) {
+                            return "Not Weird";
+                        } 
+                        return "Weird";
+                    }
+                    return "Not Weird";
+                    }
+               return "Not Weird";
+            } 
+            else if(input % 2 == 1 ) {
+                return "Weird";
+            }
+        }
         return null;
     }
 }
--- src/main/java/com/stayready/as03/problem03/Problem03.java
+++ src/main/java/com/stayready/as03/problem03/Problem03.java
@@ -1,7 +1,17 @@
 package com.stayready.as03.problem03;
 
+import java.util.ArrayList;
+
 public class Problem03 {
     public String isPalindrome(String input){
+        ArrayList<Character> palindrome = new ArrayList<>();
+        for(int i = 0; i < input.length(); i++) {
+            if(palindrome.equals(input.charAt(i))) {
+                palindrome.remove((Character)input.charAt(i));
+            } else {
+                palindrome.add(input.charAt(i));
+            }
+        }
         return null;
     }
 }
