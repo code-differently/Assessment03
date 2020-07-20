4fd523c62e4fc514151dbc9f171bcf80b102a0fd
Kaveesha <weerasirikaveesha@gmail.com>
2020-07-20 12:35:54 -0500

    finished question 3

--- src/main/java/com/stayready/as03/problem03/Problem03.java
+++ src/main/java/com/stayready/as03/problem03/Problem03.java
@@ -1,7 +1,28 @@
 package com.stayready.as03.problem03;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem03 {
     public String isPalindrome(String input){
-        return null;
+        HashMap<Character, Integer> numTimes = new HashMap<>();
+
+        for(char letter: input.toCharArray()) {
+            if(numTimes.containsKey(letter)) {
+                numTimes.put(letter, numTimes.get(letter) + 1);
+            }
+            else {
+                numTimes.put(letter, 1);
+            }
+        }
+
+        int countOdds = 0;
+        for(Map.Entry<Character, Integer> element: numTimes.entrySet()) {
+            if(element.getValue() % 2 == 1) {
+                countOdds++;
+            }
+        }
+        
+        return countOdds > 1 ? "NO" : "YES";
     }
 }
