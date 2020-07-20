236e19daa066f0dd72e2594a2af394771622c973
Kaiyen Thompson <kthompson6300@gmail.com>
2020-07-20 18:13:25 -0400

    ready for review

--- src/main/java/com/stayready/as03/problem01/Problem01.java
+++ src/main/java/com/stayready/as03/problem01/Problem01.java
@@ -3,6 +3,10 @@ package com.stayready.as03.problem01;
 public class Problem01 {
 
     public String isThisWeird(int input){
-        return null;
+        int num= 1;
+        if(num % 1 == 0)
+           return  "Weird";
+        else
+            return  " Not Wierd";
     }
 }
