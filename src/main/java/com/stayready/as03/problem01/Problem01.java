49e2d8b7b348a38f799ce65fa3c44ab105d424d6
Brent Oliver <balexanderoliver@gmail.com>
2020-07-20 12:31:19 -0400

    Updated Problem01

--- src/main/java/com/stayready/as03/problem01/Problem01.java
+++ src/main/java/com/stayready/as03/problem01/Problem01.java
@@ -7,6 +7,8 @@ public class Problem01 {
             return "Weird";
         else if(input >= 2 && input <= 5)
             return "Not Weird";
+        else if(input >= 6 && input <= 20)
+            return "Weird";
         else if(input > 20)
             return "Not Weird";
         return null;
