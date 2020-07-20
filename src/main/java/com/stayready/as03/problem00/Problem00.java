43cd7ef74343e093dd98e2d16c3bdfd90c3c398c
Lucious Morrison <lmorrison1116@gmail.com>
2020-07-20 14:39:50 -0400

    finsihed assessment

--- src/main/java/com/stayready/as03/problem00/Problem00.java
+++ src/main/java/com/stayready/as03/problem00/Problem00.java
@@ -1,7 +1,27 @@
 package com.stayready.as03.problem00;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem00 {
     public String countUniqueWords(String input){
-        return null;
+        // JUnit expected output is too hardcoded
+
+        Map<String, Integer> hm = new HashMap<String, Integer>();
+        input = input.replaceAll("\\p{Punct}", "");
+        String[] words = input.split(" ");
+        for (String s : words) {
+            if (hm.containsKey(s)) {
+                hm.put(s, hm.get(s) + 1);
+            } else {
+                hm.put(s, 1);
+            }
+        }
+        StringBuilder output = new StringBuilder("The ");
+        output.append(hm.size() + " unique words are:");
+        for (Map.Entry<String, Integer> entry : hm.entrySet()) {
+            output.append("\n" + entry.getKey() + " (Seen " + entry.getValue() + ")");
+        }
+        return output.toString();
     }
 }
--- src/main/java/com/stayready/as03/problem03/Problem03.java
+++ src/main/java/com/stayready/as03/problem03/Problem03.java
@@ -2,6 +2,20 @@ package com.stayready.as03.problem03;
 
 public class Problem03 {
     public String isPalindrome(String input){
-        return null;
+        int[] alphabet = new int[26];
+        int count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            alphabet[input.charAt(i) - 'a']++;
+        }
+        for (int num : alphabet) {
+            if (num % 2 != 0) {
+                count++;
+            }
+            if (count > 1) {
+                return "NO";
+            }
+
+        }
+        return "YES";
     }
 }
