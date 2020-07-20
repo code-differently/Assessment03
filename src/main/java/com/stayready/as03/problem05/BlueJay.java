acf9b1284864565b3d67899cefb1a0c7daaa83bc
KJ Witherspoon <50391760+Spooncodes@users.noreply.github.com>
2020-07-20 19:35:39 -0400

    Still my set like CT

--- src/main/java/com/stayready/as03/problem05/BlueJay.java
+++ src/main/java/com/stayready/as03/problem05/BlueJay.java
@@ -1,13 +1,14 @@
 package com.stayready.as03.problem05;
 
 public class BlueJay extends Bird {
-    @Override
-    public int getSpeed() {
-        return 13;
-    }
 
     @Override
     public String color() {
         return "blue";
+    }
+
+    @Override
+    public int getSpeed() {
+        return 13;
     }
 }
