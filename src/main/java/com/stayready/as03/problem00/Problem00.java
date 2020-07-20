c046b83ffd98925d6b3ea3daf14fbc2631b983ec
Kaveesha <weerasirikaveesha@gmail.com>
2020-07-20 12:06:53 -0500

    did part one, had to change the unit test so it conforms with each other

--- src/main/java/com/stayready/as03/problem00/Problem00.java
+++ src/main/java/com/stayready/as03/problem00/Problem00.java
@@ -1,7 +1,43 @@
 package com.stayready.as03.problem00;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem00 {
-    public String countUniqueWords(String input){
-        return null;
+    public String countUniqueWords(String input) {
+        int whereInTheString = 0;
+        HashMap<Integer, HashMap<String, Integer>> countOccurances = new HashMap<>();
+        HashMap<String, Integer> whereInTheBigWord = new HashMap<>();
+        String[] words = input.split("\\W");
+
+        for (String word : words) {
+            if (whereInTheBigWord.containsKey(word)) {
+                int index = whereInTheBigWord.get(word);
+                countOccurances.get(index).put(word, countOccurances.get(index).get(word) + 1);
+            }
+            else if(!word.equals("")) {
+                whereInTheBigWord.put(word, whereInTheString);
+                //need to have that position not null
+                countOccurances.put(whereInTheString, new HashMap<String, Integer>());
+                countOccurances.get(whereInTheString).put(word, 1);
+                whereInTheString++;
+            }
+        }
+
+        StringBuilder answer = new StringBuilder();
+        answer.append("The " + countOccurances.size() + " unique words are:\n");
+
+        for (Map.Entry<Integer, HashMap<String, Integer>> indexes : countOccurances.entrySet()) {
+            for(Map.Entry<String, Integer> count: indexes.getValue().entrySet()) {
+                if(indexes.getKey() != whereInTheString - 1) {
+                    answer.append(count.getKey() + " (Seen " + count.getValue() + ")\n");
+                }
+                else {
+                    answer.append(count.getKey() + " (Seen " + count.getValue() + ")");
+                }
+            }
+        }
+
+        return answer.toString();
     }
 }
