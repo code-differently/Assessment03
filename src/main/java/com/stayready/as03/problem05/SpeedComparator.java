f713d34a265d6b5df0d9e6891ead818957b41b67
Kaveesha <weerasirikaveesha@gmail.com>
2020-07-20 13:22:39 -0500

    finished test

--- src/main/java/com/stayready/as03/problem05/SpeedComparator.java
+++ src/main/java/com/stayready/as03/problem05/SpeedComparator.java
@@ -1,4 +1,10 @@
 package com.stayready.as03.problem05;
 
-public class SpeedComparator {
+import java.util.Comparator;
+
+public class SpeedComparator implements Comparator<Animal> {
+    @Override
+    public int compare(Animal animal1, Animal animal2) {
+        return animal2.getSpeed() - animal1.getSpeed();
+    }
 }
