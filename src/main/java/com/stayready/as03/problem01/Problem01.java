3f22c8a179cd2a8bb9b395c028b039a8320f93a1
Kamglitchd <skamara2@umd.edu>
2020-07-20 13:39:39 -0400

    Finished Assessment

--- src/main/java/com/stayready/as03/problem01/Problem01.java
+++ src/main/java/com/stayready/as03/problem01/Problem01.java
@@ -2,7 +2,18 @@ package com.stayready.as03.problem01;
 
 public class Problem01 {
 
-    public String isThisWeird(int input){
-        return null;
+    public String isThisWeird(int input) {
+        if (input % 2 == 1) {
+            return "Weird";
+        } else {
+            if (input <= 5 && input >= 2) {
+                return "Not Weird";
+            }
+            if (input <= 20 && input >= 6) {
+                return "Weird";
+            } else {
+                return "Not Weird";
+            }
+        }
     }
 }
--- src/main/java/com/stayready/as03/problem02/Problem02.java
+++ src/main/java/com/stayready/as03/problem02/Problem02.java
@@ -1,10 +1,20 @@
 package com.stayready.as03.problem02;
 
+import java.util.HashSet;
+
 public class Problem02 {
 
     public int findNumberOfNonMatching(int[] ar) {
-        return 0;
-    }
+            HashSet<Integer> socks = new HashSet<>();//only holds unique values
+            int pairs = 0;//counter
+        for (int value : ar) {//goes through the array
+            if (!socks.add(value)) {//if it can't be added to set
+                socks.remove(value);//remove it to find other pairs of same color
+                pairs++;//count pairs
+            }
+        }
+            return pairs;
+        }
 
 }
 
