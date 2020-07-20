64321c48b13c3c879a24f955669d74ceada0105f
Brandon Daughtry <bdaughtr@aggies.ncat.edu>
2020-07-20 16:47:13 -0400

    Problem 1 passed

--- src/main/java/com/stayready/as03/problem00/Problem00.java
+++ src/main/java/com/stayready/as03/problem00/Problem00.java
@@ -1,7 +1,20 @@
 package com.stayready.as03.problem00;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Problem00 {
     public String countUniqueWords(String input){
-        return null;
+
+        String[] strArr = input.split("\\s+");
+        Map<String,String> uniqueMap = new LinkedHashMap<String, String>();
+
+        for(int i = 0; i<strArr.length; i++){
+            if(uniqueMap.containsKey(strArr[i]))
+                uniqueMap.put(strArr[i], "Unique");
+
+        }
+
+        return uniqueMap.toString();
     }
 }
