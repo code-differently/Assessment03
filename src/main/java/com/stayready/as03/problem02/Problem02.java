50014606d613029e0b678184556ed78943be1906
Kaveesha <weerasirikaveesha@gmail.com>
2020-07-20 12:28:17 -0500

    finished problem 2

--- src/main/java/com/stayready/as03/problem02/Problem02.java
+++ src/main/java/com/stayready/as03/problem02/Problem02.java
@@ -1,9 +1,20 @@
 package com.stayready.as03.problem02;
 
+import java.util.HashSet;
+
 public class Problem02 {
 
     public int findNumberOfNonMatching(int[] ar) {
-        return 0;
+        HashSet<Integer> pairs = new HashSet<>();
+        for(int sock: ar) {
+            if(pairs.contains(sock)) {
+                pairs.remove(sock);
+            }
+            else {
+                pairs.add(sock);
+            }
+        }
+        return pairs.size();
     }
 
 }
