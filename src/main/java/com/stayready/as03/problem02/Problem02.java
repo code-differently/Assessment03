137a262102e979caf3b433386f59bf59f2da546c
toribreeana <tori.bree@outlook.com>
2020-07-20 22:24:50 -0400

    SOS, I tried my best

--- src/main/java/com/stayready/as03/problem02/Problem02.java
+++ src/main/java/com/stayready/as03/problem02/Problem02.java
@@ -1,10 +1,24 @@
 package com.stayready.as03.problem02;
 
+import java.util.Arrays;
+
 public class Problem02 {
 
     public int findNumberOfNonMatching(int[] ar) {
         return 0;
-    }
 
+        int PairOfSocks = 0;
+        Arrays.sort(ar);
+
+        for(int i = 0; i < ar.length; i++){
+            if(ar[i] == ar[i + 1]){
+                i++;
+                PairOfSocks++;
+
+            }
+        }
+
+    }
+        return PairOfSocks;
 }
 
