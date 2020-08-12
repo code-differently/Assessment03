557d00f5a41f699d68e75bd2c000ad1b50ae1588
Kaveesha <weerasirikaveesha@gmail.com>
2020-08-11 21:25:12 -0500

    made changes

--- src/main/java/com/stayready/as03/problem03/Problem03.java
+++ src/main/java/com/stayready/as03/problem03/Problem03.java
@@ -1,27 +1,24 @@
 package com.stayready.as03.problem03;
 
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Problem03 {
-    HashMap<Character, Integer> numTimes = new HashMap<>();
-    public String isPalindrome(String input){
-
-
-        for(char letter: input.toCharArray()) {
-                numTimes.merge(letter, 1, Integer::sum);
-        }
-
-        return findNumberOfOddOccurrences() <= 1 ? "YES" : "NO";
+    public String isPalindrome(String input) {
+        int occurrenceThreshold = 1;
+        return numberOfLettersAppearingAnOddNumberOfTimes(input) <= occurrenceThreshold ? "YES" : "NO";
     }
 
-    private int findNumberOfOddOccurrences() {
-        AtomicInteger countOdds = new AtomicInteger();
-        numTimes.forEach((key, value) -> {
-            if(value % 2 == 1) {
-                countOdds.getAndIncrement();
+    private int numberOfLettersAppearingAnOddNumberOfTimes(String input) {
+        HashSet<Character> lettersOccurringAnOddNumberOfTimes = new HashSet<>();
+        for(char letter: input.toCharArray()) {
+            if(lettersOccurringAnOddNumberOfTimes.contains(letter)) {
+                lettersOccurringAnOddNumberOfTimes.remove(letter);
             }
-        });
-        return countOdds.get();
+            else {
+                lettersOccurringAnOddNumberOfTimes.add(letter);
+            }
+        }
+        return lettersOccurringAnOddNumberOfTimes.size();
     }
 }
